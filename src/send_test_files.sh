3596b018e92fa6db48ba335c63182157d9f131f6
Tim <tim.lustberg@medicaldataworks.nl>
2019-02-14 17:06:24 +0100

    wrote the manual and updated some scripts

--- src/send_test_files.sh
+++ src/send_test_files.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+#
+#configuration (required: https://stedolan.github.io/jq/download/)
+config_file="pipeline_config.json"
+dicomport="$( jq -r '.dicomport' "$config_file" )"
+#
+# send 10 files to test the pipeline
+# offis dcmtk required! https://dicom.offis.de/dcmtk.php.en
 for i in {1..10}
 do
 dcmodify -nb -gin "orig.dcm"
-storescu -v localhost 104 "orig.dcm"
+storescu -v localhost $dicomport "orig.dcm"
 done
